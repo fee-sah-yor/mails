61a866ad2658947dd4c84499f01f04003e924e4c
Theodore Imonigie <theodoreimonigie@gmail.com>
2024-08-09 06:46:45 +0100

    🔧 react-email : 2024-08-09 06:46:45 🔧

---
+++ emails/header.tsx
@@ -0,0 +1,17 @@
+import { Img, Section } from "@react-email/components";
+import React from "react";
+import { resources } from "./resources";
+
+export const Header = ({
+  hostUrl = resources.base,
+}) => {
+  const baseUrl = hostUrl + resources.basePath;
+
+  return <Section className="bg-black p-5 ">
+    <Img
+      className="h-6"
+      src={`${baseUrl}${resources.logo}`}
+      alt="Shuttrd"
+    />
+  </Section>
+}
---
+++ emails/resources.ts
@@ -0,0 +1,14 @@
+export const resources = {
+  logo: '/images/logo.png',
+  logoWhite: '/images/logo-white.png',
+  facebookLogo: '/images/facebook.png',
+  linkdinLogo: '/images/linkedin.png',
+  instagramLogo: '/images/instagram.png',
+  basePath: '/view/resources',
+  base: 'https://j9jdvz85-4000.euw.devtunnels.ms',
+  live: 'https://www.slack.com',
+  headerFont: '/fonts/Montserrat-Bold.woff2',
+  bodyFont: '/fonts/lato-Regular.woff2',
+  textFont: '/fonts/Roboto.woff2',
+}
+
